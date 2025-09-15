2589f7cd3c49321b76721a8df8c876a29cde5bd8
vipasha-joshi <98696027+vipasha-joshi@users.noreply.github.com>
2025-09-15 11:21:08 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node API!' });
+  res.json({ message: 'Hello from Node JS API!' });
 });
 
 const PORT = process.env.PORT || 3001;
