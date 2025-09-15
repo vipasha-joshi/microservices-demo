7dd6b4a9e4862e082cf31fc9e9227494a4c36872
vipasha-joshi <98696027+vipasha-joshi@users.noreply.github.com>
2025-09-15 12:18:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Hi, Vipasha!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>       
       </div>
       <p className="read-the-docs">
